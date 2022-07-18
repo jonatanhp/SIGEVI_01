18dfd29c9c888f79df67fcbf151e7245df165bf2
jonatanhp <jonatan.hp@upeu.edu.pe>
2022-07-18 16:59:21 -0500

    auth_screens

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:sigevi_1/models/user.dart';
+import 'package:sigevi_1/resources/auth_helper.dart';
+
+class UserProvider with ChangeNotifier {
+  User? _user;
+  final AuthMethods _authMethods = AuthMethods();
+
+  User get getUser => _user!;
+
+  Future<void> refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
